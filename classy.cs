de8b02ba8c95521c7a26ea2fb9840b72548468a2
ccarter92 <54338971+ccarter92@users.noreply.github.com>
2019-09-30 14:50:15 -0500

    Add files via upload

---
+++ classy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AnimalHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+           
+
+        }
+    } 
+   
+    class animal
+    {
+        String[] Dbreed = { "Husky", "Pitbull", "Rockweiler" };
+        string[] Cbreed = { "Six-toed","Brown", "Black", "White", "Mixed" };
+        string[] Cname = { "Shadow", "Willow", "Tinly", "Dots" };
+        string[] DName = { "Bucky", "Chuck", "Beetovon", "Chunky" };
+        bool cat = true;
+        
+        
+          
+        
+    }
+    class dog
+    {
+
+        public string DName { get; set; }
+        public string _DBreed { get; set; }
+
+    }
+
+        class cat
+    {
+        public string Cname { get; set; }
+        public string _CBreed { get; set; }
+        
+    }
+
+   
+   
+    
+
+}
